5a3fdb62e760304792041952a30945fe4f05704b
JuliaGrigorova <y_grigorova@inbox.ru>
2020-11-14 02:16:44 +0300

    Create Сложение двух массивов.cpp

---
+++ дз сложение двух массивов/Сложение двух массивов.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#define N 10
+
+int main(void)
+{
+	setlocale(LC_ALL, "Rus");
+
+	int mas_A[N];
+	int mas_B[N];
+	int mas_sum[N];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < N; i++) {
+		mas_A[i] = rand() % 100;
+		printf("%d ", mas_A[i]);
+	}
+
+	printf("\n");
+
+	for (int i=0;i<N;i++) {
+		mas_B[i] = rand() % 100;
+		printf("%d ", mas_B[i]);
+	}
+	printf("\n");
+
+	printf("Сумма: ");
+	for (int i = 0; i < N; i++) {
+		mas_sum[i] = mas_A[i] + mas_B[i];
+		printf("%d ", mas_sum[i]);
+	}
+
+	printf("\n");
+	return 0;
+}
